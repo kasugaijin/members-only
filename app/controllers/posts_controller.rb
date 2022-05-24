3a6e5261d9b16df58c26ab1d5fdb34d1ea0f9c07
Ben <robinsonbena@gmail.com>
2022-05-23 19:15:52 -0700

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+
+  # before action to limit create and new actions
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  resources :posts
+
+  root "posts#index"
+end
