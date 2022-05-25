19b79145e47581967518f7882195e30de9808fbd
Ben <robinsonbena@gmail.com>
2022-05-25 16:09:38 -0700

    app complete, added styles to all pages, made tiles with view post links and style show page for posts, still has log in failure error where it stalls - a known rails 7 + devise issue

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
+  validates :body, presence: true, length: { minimum: 10, maximum: 100 }
 end
